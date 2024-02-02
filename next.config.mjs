05fe33a1f97d1f9e4c9e3cc7520d75a242920a80
Marina Tanaka <huttenloucher@gmail.com>
2024-02-02 13:04:30 -0300

    Revert "configurações pwa"
    This reverts commit f9edfa5d8b17e0efc2efbd29f86c6facedb9d6f9.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
-
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
 };
-module.exports = withPWA(nextConfig);
+
+export default nextConfig;
