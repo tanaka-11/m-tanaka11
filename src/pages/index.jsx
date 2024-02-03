ce4dcfa01acb918211b7928cf76cdae2743e77f2
Marina Tanaka <huttenloucher@gmail.com>
2024-02-03 10:15:50 -0300

    teste do hover inicial começar com filtro

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -50,16 +50,19 @@ const StyledHome = styled.section`
   }
 
   img {
-    width: 100%;
-    height: auto;
-    max-width: 100%;
-    transition: transform 0.7s ease, filter 2s ease;
-    filter: grayscale(100%);
+    & #element,
+    &.initial-hover {
+      width: 100%;
+      height: auto;
+      max-width: 100%;
+      transition: transform 0.7s ease, filter 2s ease;
+      filter: grayscale(100%);
 
-    &:hover,
-    &:focus {
-      transform: scale(1.1);
-      filter: grayscale(65%);
+      &:hover,
+      &:focus {
+        transform: scale(1.1);
+        filter: grayscale(65%);
+      }
     }
   }
 
@@ -158,6 +161,18 @@ export default function Home({ mensagens }) {
     setFraseAtual(fraseAleatoria);
   }, []);
 
+  // Hook para o Hover
+  useEffect(() => {
+    const addInitialHover = () => {
+      const element = document.getElementById("element");
+      if (element) {
+        element.classList.add("initial-hover");
+      }
+    };
+
+    addInitialHover();
+  }, []);
+
   return (
     <>
       <Head>
@@ -167,7 +182,7 @@ export default function Home({ mensagens }) {
       <StyledHome>
         {fraseAtual && <h3>"{fraseAtual.mensagem}"</h3>}
 
-        <Container>
+        <Container id="element" className="initial-hover">
           <article>
             <Image
               src="/images/demonio.jpg"
