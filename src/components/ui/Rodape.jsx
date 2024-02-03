b516d74e857590e43b1db8f44fc1d4aa5f65a9a1
Marina Tanaka <huttenloucher@gmail.com>
2024-02-03 10:54:27 -0300

    Update Rodape.jsx

--- src/components/ui/Rodape.jsx
+++ src/components/ui/Rodape.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 // CSS
@@ -16,27 +16,21 @@ const StyledRodape = styled.footer`
 
 export default function Rodape() {
   const router = useRouter();
+  const [textColor, setTextColor] = useState("#edeff0ee"); // Cor padrão
 
   useEffect(() => {
     // Lógica para definir a cor do footer com base na página atual
-    let color = "#edeff0ee"; // Cor padrão
-
     if (router.pathname === "/") {
-      color = "#d7598b";
+      setTextColor("#d7598b");
     } else if (router.pathname === "/hobbys") {
-      color = "#7c7cbd";
+      setTextColor("#7c7cbd");
     } else if (router.pathname === "/contato") {
-      color = "#2aa3db";
+      setTextColor("#2aa3db");
     }
-
-    // Atualize a propriedade textColor no StyledRodape
-    document
-      .getElementById("meuRodape")
-      .style.setProperty("--text-color", color);
   }, [router.pathname]);
 
   return (
-    <StyledRodape id="meuRodape" textColor={color}>
+    <StyledRodape textColor={textColor}>
       <h3>Direitos Reservados ©</h3>
     </StyledRodape>
   );
