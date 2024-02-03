02f9be069469f1cb5b643b81e223cd1a2bcd7db9
Marina Tanaka <huttenloucher@gmail.com>
2024-02-03 10:26:29 -0300

    Update Rodape.jsx

--- src/components/ui/Rodape.jsx
+++ src/components/ui/Rodape.jsx
@@ -3,18 +3,29 @@ import styled from "styled-components";
 const StyledRodape = styled.footer`
   background-color: var(--cor-preto);
   color: var(--cor-primaria);
+  box-shadow: var(--sombra-box);
   height: 10vh;
   padding: 1rem;
   text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  span {
+    color: var(--cor-quartenaria);
+  }
+
+  h3 {
+    color: var(--cor-terciaria);
+  }
 `;
 
 export default function Rodape() {
   return (
     <StyledRodape>
-      <h3 style={{ textAlign: "center" }}>Direitos Reservados ©</h3>
+      <h3>
+        Direitos Reservados <span>©</span>
+      </h3>
     </StyledRodape>
   );
 }
