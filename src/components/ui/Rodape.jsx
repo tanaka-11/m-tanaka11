e49109e029499f2e468ceea832b16905a96fa364
Marina Tanaka <huttenloucher@gmail.com>
2024-02-03 10:42:55 -0300

    programação para footer colorido em cada pagina

--- src/components/ui/Rodape.jsx
+++ src/components/ui/Rodape.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
+// CSS
 const StyledRodape = styled.footer`
   background-color: var(--cor-preto);
   color: var(--cor-primaria);
@@ -13,8 +16,28 @@ const StyledRodape = styled.footer`
 `;
 
 export default function Rodape() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Lógica para definir a cor do footer com base na página atual
+    let color = "#edeff0ee"; // Cor padrão
+
+    if (router.pathname === "/") {
+      color = "#d7598b";
+    } else if (router.pathname === "/hobbys") {
+      color = "#7c7cbd";
+    } else if (router.pathname === "/contato") {
+      color = "#2aa3db";
+    }
+
+    // Defina a cor usando styled-components props
+    document
+      .getElementById("meuRodape")
+      .style.setProperty("--text-color", color);
+  }, [router.pathname]);
+
   return (
-    <StyledRodape>
+    <StyledRodape id="meuRodape" textColor="#edeff0ee">
       <h3>Direitos Reservados ©</h3>
     </StyledRodape>
   );
